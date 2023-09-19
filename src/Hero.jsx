36a80c1d3e614118172e97b0ccdd456f884786d8
Sijan Malla <sijan.malla101@gmail.com>
2023-09-19 13:13:48 -0500

    Added Nested Components and Hero Component:

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import heroImg from './assets/hero.svg';
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>contentful CMS</h1>
+          <p>
+            Contentful is a tool to create captivating experiences with
+            intelligent composable content platform.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="A lady and Browser" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>Projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center"></div>
+    </section>
+  );
+};
+
+export default Projects;
